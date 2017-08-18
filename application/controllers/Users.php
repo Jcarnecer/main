d144da6f170df7d7ba3ab6f9347607351e8ff7d7
Christian Jordan Dalan <cjdalan@gmail.com>
2017-08-18 14:01:49 +0800

    Added CORS

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -4,6 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Users extends CI_Controller {
 
 	public function __construct() {
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Headers: *");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
 		parent::__construct();
 	}
 
