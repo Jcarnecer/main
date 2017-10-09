a4ae433d52e635a903c650b0b8af9cf3a3bcf6b4
Christian Jordan Dalan <cjdalan@gmail.com>
2017-10-09 13:57:03 +0800

    Updated base url on all pages

--- application/views/company/register.php
+++ application/views/company/register.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 		<title>Simple Apps</title>
-		<base href="http://localhost/login/" />
+		<base href="<?= base_url() ?>" />
 		<link rel="stylesheet" type="text/css" href="assets/css/flavored-reset-and-normalize.css" >
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" >
 		<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css" >
