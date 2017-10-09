2ba82aa2999f94edb640da493e31baf250553331
Christian Jordan Dalan <cjdalan@gmail.com>
2017-10-09 10:33:14 +0800

    Updated base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-#$config['base_url'] = 'https://login-simpleapps.herokuapp.com/';
-$config['base_url'] = 'http://localhost/login';
+$config['base_url'] = 'http://localhost/main';
 /*
 |--------------------------------------------------------------------------
 | Index File
