67f704d760f19cdde29eb798731e5961ca088a6a
Christian Jordan Dalan <cjdalan@gmail.com>
2017-10-09 13:53:31 +0800

    Updated .htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/main';
+if (ENVIRONMENT == "production") {
+	$config['base_url'] = 'http://52.76.142.14/main/';
+} else {
+	$config['base_url'] = 'http://localhost/main/';
+}
+
 /*
 |--------------------------------------------------------------------------
 | Index File
