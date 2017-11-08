47416699b23b3f3a68de4469c6ede8b42f8ea8c2
Christian Jordan Dalan <cjdalan@gmail.com>
2017-11-08 18:39:46 +0800

    Update profile view

--- application/views/users/profile.php
+++ application/views/users/profile.php
@@ -4,18 +4,31 @@
 			<div class="card">
 				<div class="card-header">My Profile</div>
 				<div class="card-body">
-					<div class="row">
-						<div class="col-sm-2">Name</div>
-						<div class="col-sm-8">
-							<?= $user->first_name . " " . $user->last_name ?>
+					<dl class="row">
+						<div class="col-sm-2"></div>
+						<div class="col-sm-2">
+							<img class="img-fluid" src="<?= base_url("assets/img/avatar/default.png") ?>" />
 						</div>
+					</dl>
+					<dl class="row">
+						<div class="col-sm-2 text-right">Name</div>
+						<div class="col-sm-8"><?= $user->first_name ?> <?= $user->last_name ?></div>
 						<div class="col-sm-2"><a class="" href="">Edit</a></div>
-					</div>
-					<div class="row">
-						<div class="col-sm-2">Password</div>
+					</dl>
+					<dl class="row">
+						<div class="col-sm-2 text-right">E-mail Address</div>
+						<div class="col-sm-8"><?= $user->email_address ?></div>
+						<div class="col-sm-2"><a class="" href="">Edit</a></div>
+					</dl>
+					<dl class="row">
+						<div class="col-sm-2 text-right">Password</div>
 						<div class="col-sm-8">********</div>
 						<div class="col-sm-2"><a class="" href="">Edit</a></div>
-					</div>
+					</dl>
+					<dl class="row">
+						<div class="col-sm-2 text-right">Role</div>
+						<div class="col-sm-10"><?= $user->role ?></div>
+					</dl>
 				</div>
 			</div>
 		</div>
