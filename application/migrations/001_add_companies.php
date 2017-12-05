8acdc25559b5d13a19f148210155766af3d8985f
Christian Jordan Dalan <cjdalan@gmail.com>
2017-12-05 15:08:30 +0800

    Remove ci_sessions migration

--- application/migrations/001_add_companies.php
+++ application/migrations/001_add_companies.php
@@ -5,37 +5,11 @@ class Migration_Add_Companies extends CI_Migration {
 
 
 	public function up() {
-		$this->sessions();
 		$this->companies();
 	}
 
 	public function down() {
 		$this->dbforge->drop_table("companies", TRUE);
-		$this->dbforge->drop_table("ci_sessions", TRUE);
-	}
-
-	public function sessions() {
-		$this->dbforge->add_field([
-			"id" => [
-				"type" => "VARCHAR",
-				"constraint" => 128
-			],
-			"ip_address" => [
-				"type" => "VARCHAR",
-				"constraint" => 45
-			],
-			"timestamp" => [
-				"type" => "INT",
-				"constraint" => 10,
-				"default" =>  "0"
-			],
-			"data" => [
-				"type" => "BLOB"
-			]
-		]);	
-		
-		$this->dbforge->add_key('id', TRUE);
-		$this->dbforge->create_table('ci_sessions');
 	}
 
 	public function companies() {
