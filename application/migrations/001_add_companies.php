6f6399bef4efcb829319afc38bad8eca3682456c
Christian Jordan Dalan <cjdalan@gmail.com>
2017-12-05 15:11:21 +0800

    Add date_default_timezone_set

--- application/migrations/001_add_companies.php
+++ application/migrations/001_add_companies.php
@@ -11,31 +11,6 @@ class Migration_Add_Companies extends CI_Migration {
 
 	public function down() {
 		$this->dbforge->drop_table("companies", TRUE);
-		$this->dbforge->drop_table("ci_sessions", TRUE);
-	}
-
-	public function sessions() {
-		$this->dbforge->add_field([
-			"id" => [
-				"type" => "VARCHAR",
-				"constraint" => 128
-			],
-			"ip_address" => [
-				"type" => "VARCHAR",
-				"constraint" => 45
-			],
-			"timestamp" => [
-				"type" => "INT",
-				"constraint" => 10,
-				"default" =>  "0"
-			],
-			"data" => [
-				"type" => "BLOB"
-			]
-		]);	
-		
-		$this->dbforge->add_key('id', TRUE);
-		$this->dbforge->create_table('ci_sessions');
 	}
 
 	public function companies() {
