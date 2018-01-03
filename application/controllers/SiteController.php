b7e6014be6bb7523220ac241e0c48533b71fa131
Christian Jordan Dalan <cjdalan@gmail.com>
2018-01-03 16:50:22 +0800

    Cleaned SiteController

--- application/controllers/SiteController.php
+++ application/controllers/SiteController.php
@@ -1,20 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SiteController extends BaseController {
+class SiteController extends BaseController
+{
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct();
 	}
 
-	public function index() {
-		$user = parent::current_user();
 
-		if ($user) {
+	public function index()
+	{
+		if (parent::current_user()) {
 			parent::main_page("dashboard");
 		} else {
-			$this->load->view("welcome");
+			parent::guest_page("welcome");
 		}
-		
 	}
 }
