10bfcabede2d7a774b162ba4601e7cd0c41cf085
aereneth <zerify@gmail.com>
2018-02-07 18:01:11 +0800

    Time

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,63 +1,70 @@
-<div class="container-fluid py-3">
+<div class="container-fluid d-flex flex-column p-0">
     
-    <?php if($remaining_day < 7): ?>
-    <div class="card card-notif mb-3">
-        <div class="card-header bg-primary" style="overflow: auto;">
-            <span class="h5 font-weight-bold">Notice</span>
-            <button class="btn btn-danger btn-sm d-inline-block float-right" onclick="closeNotif(event)"><i class="fa fa-close"></i></button>
+    <div id="notif" class="w-100">
+        <?php if($remaining_day < 7): ?>
+        <div class="card card-notif mt-1 mb-3">
+            <div class="card-header bg-primary" style="overflow: auto;">
+                <span class="h5 font-weight-bold">Notice</span>
+                <button class="btn btn-danger btn-sm d-inline-block float-right" onclick="closeNotif(event)"><i class="fa fa-close"></i></button>
+            </div>
+            <div class="card-body">
+                <span>Your free trial will end at <?= $remaining_day ?> day(s).</span>
+            </div>
         </div>
-        <div class="card-body">
-            <span>Your free trial will end at <?= $remaining_day ?> day(s).</span>
-        </div>
+        <?php endif; ?>
     </div>
-    <?php endif; ?>
 
-    <h1 class="mt-4 text-center">Hello, <?= $first_name ?></h1>
-    <h5 class="mb-5 text-center">We picked up where you left.</h5>
+    <div class="bg-primary text-white w-100 py-3">
+        <h1 class="mt-4 text-center"><span id="greetings"></span></h1>
+        <h5 class="text-center">Today is <span id="date" class="font-weight-bold"></span></h5>
+        <h5 class="text-center">The time is <span id="clock" class="font-weight-bold"></span></h5>
+    </div>
 
-    <div class="row align-items-stretch my-3">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header h4"><i class="fa fa-tasks"></i> Today's Task</div>
-                <div class="card-body">
-                    <div id="taskToday" class="card-columns">
-                    
+    <div class="w-100 h-100 px-3 py-4">
+        <div class="row align-items-stretch my-3">
+            <div class="col-md-8">
+                <div class="card h-100">
+                    <div class="card-header h5"><i class="fa fa-tasks"></i> Today's Task</div>
+                    <div class="card-body">
+                        <div id="taskToday" class="card-columns">
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card h-100">
+                    <div class="card-header h5"><i class="fa fa-bullhorn"></i> Announcements</div>
+                    <div id="announcement" class="card-body">
+
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header h4"><i class="fa fa-bullhorn"></i> Announcements</div>
-                <div id="announcement" class="card-body">
-
+        <div class="card-columns w-100 my-3">
+            <!-- <div class="card" onclick="goto('chat')">
+                <div class="card-body">
+                    <h4><i class="fa fa-comments"></i> Chat</h4>
+                </div>
+            </div> -->
+            <div class="card app-card" onclick="goto('task')">
+                <div class="card-body">
+                    <h5><i class="fa fa-tasks"></i> Task <span id="taskNotif" class="badge badge-dark float-right"></span></h5>
+                </div>
+            </div>
+            <div class="card app-card" onclick="goto('forum')">
+                <div class="card-body">
+                    <h5><i class="fa fa-exchange"></i> Forum</h5>
+                </div>
+            </div>
+            <div class="card app-card" onclick="goto('note')">
+                <div class="card-body">
+                    <h5><i class="fa fa-sticky-note"></i> Note <span id="noteNotif" class="badge badge-dark float-right"></span></h5>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="card-columns w-100 my-3">
-        <!-- <div class="card" onclick="goto('chat')">
-            <div class="card-body">
-                <h4><i class="fa fa-comments"></i> Chat</h4>
-            </div>
-        </div> -->
-        <div class="card app-card" onclick="goto('task')">
-            <div class="card-body">
-                <h4><i class="fa fa-tasks"></i> Task <span id="taskNotif" class="badge badge-dark float-right"></span></h4>
-
-            </div>
-        </div>
-        <div class="card app-card" onclick="goto('forum')">
-            <div class="card-body">
-                <h4><i class="fa fa-exchange"></i> Forum</h4>
-            </div>
-        </div>
-        <div class="card app-card" onclick="goto('note')">
-            <div class="card-body">
-                <h4><i class="fa fa-sticky-note"></i> Note <span id="noteNotif" class="badge badge-dark float-right"></span></h4>
-            </div>
-        </div>
-    </div>
 </div>
--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,4 +1,8 @@
 $(function() {
+    $('#greetings').html(greetings());
+    $('#clock').html(clock());
+    $('#date').html(date());
+
     var task = $.ajax({
         async: false,
         type: 'GET',
@@ -18,7 +22,59 @@ $(function() {
 
     task.done(function(data) {displayTask(data)});
     note.done(function(data) {displayNote(data)});
+
+    setInterval(function() {
+        $('#greetings').html(greetings());
+        $('#clock').html(clock());
+        $('#date').html(date());
+    }, 1000)
 });
+
+function greetings() {
+    var date = new Date();
+    if (date.getHours() >= 18) {
+        return 'Good Evening';
+    } else if (date.getHours() >= 12) {
+        return 'Good Afternoon';
+    } else {
+        return 'Good Morning';
+    }
+}
+
+function date(date=null) {
+    var date = date != null ? new Date(date) : new Date();
+    var monthString = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var dayString = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    var month = date.getMonth();
+    var dayMonth = date.getDate();
+    var year = date.getFullYear();
+    var dayWeek = date.getDay();
+
+    return `${monthString[month]} ${dayMonth}, ${year} (${dayString[dayWeek]})`;
+}
+
+function clock(time=null) {
+    var date = time != null ? new Date(time) : new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var ap = 'AM';
+
+    if (hour >= 12) {
+        ap = 'PM';
+    }
+
+    if (hour > 12) {
+        hour -= 12;
+    } else if (hour === 0) {
+       hour = 12;
+    }
+
+    // hour = pad(hour, 2);
+    minute = pad(minute, 2);
+
+    return `${hour}:${minute} ${ap}`;
+}
 
 function getUrl(app) {
     return baseUrl === 'http://payakapps.com' || baseUrl === 'http://stage.payakapps.com' ? `http://${app}.payakapps.com/` : `http://localhost/${app}/`;
@@ -42,12 +98,21 @@ function displayNote(data) {
     });
 }
 
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 function taskBuilder(task) {
     var taskElem = 
-    `<div class="card" onclick="goto_task('task')">
+    `<div class="card" onclick="goto('task')">
         <div class="card-body">
             <span class="badge badge-pill rounded-circle p-2 mx-1" style="background-color:${task['color']};"> </span>
-            <h5 class="card-title mb-0 d-inline font-weight-bold">${task['title']}</h5>
+            <h6 class="card-title mb-0 d-inline font-weight-bold">${task['title']}</h6>
+        </div>
+        <div class="card-footer text-right p-1">
+            <small class="card-text font-weight-bold mb-0">${clock(task['due_date'])}</small>
         </div>
     </div>`;
 
@@ -61,7 +126,7 @@ function noteBuilder(note) {
             <h5 class="card-title mb-0 d-inline font-weight-bold">${note['title']}</h5>
             </div>
         <div class="card-footer bg-light text-right p-1">
-            <small class="card-text font-weight-bold mb-0">${note['updated_at']}</small>
+            <small class="card-text font-weight-bold mb-0">${date(note['updated_at'])} | ${clock(note['updated_at'])}</small>
         </div>
     </div>`;
 
