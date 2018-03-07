86aaed6b92717ac099fe7012f89289184b833936
jmacj <jm.nz0723@gmail.com>
2018-03-07 11:09:10 +0800

    updated subscription migration

--- application/migrations/012_add_subscription.php
+++ application/migrations/012_add_subscription.php
@@ -6,97 +6,100 @@ class Migration_Add_Subscription extends CI_Migration {
 
     public function up() {
 
-        // $this->packages();
-        // $this->modules();
-        // $this->package_details();
+        $this->packages();
+        $this->modules();
+        $this->package_details();
         $this->subscriptions();
+        $this->packages_seed();
+        $this->package_details_seed();
+        $this->modules_seed();
     }
 
 
     public function down() {
 
         $this->dbforge->drop_table('subscriptions', TRUE);
-        // $this->dbforge->drop_table('package_details', TRUE);
-        // $this->dbforge->drop_table('modules', TRUE);
-        // $this->dbforge->drop_table('packages', TRUE);
+        $this->dbforge->drop_table('package_details', TRUE);
+        $this->dbforge->drop_table('modules', TRUE);
+        $this->dbforge->drop_table('packages', TRUE);
     }
 
 
-    // public function packages() {
+    public function packages() {
 
-    //     $this->dbforge->add_field([
+        $this->dbforge->add_field([
 
-    //         'id'              => [
+            'id'              => [
 
-    //             'type'           => 'INT',
-    //             'constraint'     => 11,
-    //             'unsigned'       => TRUE,
-    //             'auto_increment' => TRUE
-    //         ],
-    //         'name'            => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'name'            => [
 
-    //             'type'           => 'VARCHAR',
-    //             'constraint'     => 50
-    //         ]
+                'type'           => 'VARCHAR',
+                'constraint'     => 50
+            ]
 
-    //     ]);
+        ]);
 
-    //     $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_key('id', TRUE);
 
-    //     return $this->dbforge->create_table('packages', TRUE);
-    // }
-
-
-    // public function modules() {
-
-    //     $this->dbforge->add_field([
-
-    //         'id'              => [
-
-    //             'type'           => 'INT',
-    //             'constraint'     => 11,
-    //             'unsigned'       => TRUE,
-    //             'auto_increment' => TRUE
-    //         ],
-    //         'name'            => [
-
-    //             'type'           => 'VARCHAR',
-    //             'constraint'     => 50
-    //         ]
-
-    //     ]);
-
-    //     $this->dbforge->add_key('id', TRUE);
-
-    //     return $this->dbforge->create_table('modules', TRUE);
-    // }
+        return $this->dbforge->create_table('packages', TRUE);
+    }
 
 
-    // public function package_details() {
+    public function modules() {
 
-    //     $this->dbforge->add_field([
+        $this->dbforge->add_field([
 
-    //         'package_id'      => [
+            'id'              => [
 
-    //             'type'           => 'INT',
-    //             'constraint'     => 11,
-    //             'unsigned'       => TRUE
-    //         ],
-    //         'module_id'       => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'name'            => [
 
-    //             'type'           => 'INT',
-    //             'constraint'     => 11,
-    //             'unsigned'       => TRUE
-    //         ],
+                'type'           => 'VARCHAR',
+                'constraint'     => 50
+            ]
 
-    //         'CONSTRAINT `package_details_ibfk_1` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE',
-    //         'CONSTRAINT `package_details_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`) ON DELETE CASCADE ON UPDATE CASCADE'
-    //     ]);
+        ]);
 
-    //     $this->dbforge->add_key(['package_id', 'module_id'], TRUE);
+        $this->dbforge->add_key('id', TRUE);
 
-    //     return $this->dbforge->create_table('package_details', TRUE);
-    // }
+        return $this->dbforge->create_table('modules', TRUE);
+    }
+
+
+    public function package_details() {
+
+        $this->dbforge->add_field([
+
+            'package_id'      => [
+
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE
+            ],
+            'module_id'       => [
+
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE
+            ],
+
+            'CONSTRAINT `package_details_ibfk_1` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE',
+            'CONSTRAINT `package_details_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`) ON DELETE CASCADE ON UPDATE CASCADE'
+        ]);
+
+        $this->dbforge->add_key(['package_id', 'module_id'], TRUE);
+
+        return $this->dbforge->create_table('package_details', TRUE);
+    }
 
 
     public function subscriptions() {
@@ -118,12 +121,12 @@ class Migration_Add_Subscription extends CI_Migration {
                 'type'           => 'VARCHAR',
                 'constraint'     => 11
             ],
-            // 'package_id'      => [
+            'package_id'      => [
 
-            //     'type'           => 'INT',
-            //     'constraint'     => 11,
-            //     'unsigned'       => TRUE0
-            // ],
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE0
+            ],
             'start_date'      => [
 
                 'type'           => 'DATE'
@@ -134,13 +137,100 @@ class Migration_Add_Subscription extends CI_Migration {
             ],
 
             'CONSTRAINT `subscriptions_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE'
-            // 'CONSTRAINT `subscriptions_ibfk_2` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE'
+            'CONSTRAINT `subscriptions_ibfk_2` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE'
         ]);
 
         $this->dbforge->add_key('id', TRUE);
         $this->dbforge->add_key('company_id');
-        // $this->dbforge->add_key('package_id');
+        $this->dbforge->add_key('package_id');
 
         $this->dbforge->create_table('subscriptions', TRUE);
+    }
+
+
+    public function packages_seed() {
+        
+        $data = [
+            [
+                'id' => '1',
+                'name' => 'Project App'
+            ],
+            [
+                'id' => '2',
+                'name' => 'Resto App'
+            ],
+            [
+                'id' => '3',
+                'name' => 'HR App'
+            ],
+            [
+                'id' => '4',
+                'name' => 'Payak Apps'
+            ]
+        ];
+
+        return $this->db->insert_batch('packages', $data);
+    }
+
+
+    public function modules_seed() {
+        
+        $data = [
+            [
+                'id' => '1',
+                'name' => 'Dashboard'
+            ],
+            [
+                'id' => '2',
+                'name' => 'Bulletin'
+            ],
+            [
+                'id' => '3',
+                'name' => 'Project'
+            ],
+            [
+                'id' => '4',
+                'name' => 'Chat'
+            ],
+            [
+                'id' => '5',
+                'name' => 'Account Management'
+            ],
+            [
+                'id' => '6',
+                'name' => 'Shift Management'
+            ],
+        ];
+
+        return $this->db->insert_batch('modules', $data);
+    }
+
+
+    public function package_details_seed() {
+        
+        $data = [
+            [
+                'package_id' => '1',
+                'module_id' => '1'
+            ],
+            [
+                'package_id' => '1',
+                'module_id' => '2'
+            ],
+            [
+                'package_id' => '1',
+                'module_id' => '3'
+            ],
+            [
+                'package_id' => '1',
+                'module_id' => '4'
+            ],
+            [
+                'package_id' => '1',
+                'module_id' => '5'
+            ],
+        ];
+
+        return $this->db->insert_batch('package_details', $data);
     }
 }
