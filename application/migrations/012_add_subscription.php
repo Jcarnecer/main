8ff2ef2b2eaefc2fb7fca445b39109cab39bd6eb
Mohammad Zahaire B. Guro <mohammadz.guro@astridtechnologies.com>
2018-03-14 14:29:08 +0800

    fixed error in migrations

--- application/migrations/012_add_subscription.php
+++ application/migrations/012_add_subscription.php
@@ -1,10 +1,11 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Migration_Add_Subscription extends CI_Migration {
+class Migration_Add_Subscription extends CI_Migration
+{
 
-
-    public function up() {
+    public function up()
+    {
 
         $this->packages();
         $this->modules();
@@ -15,252 +16,250 @@ class Migration_Add_Subscription extends CI_Migration {
         $this->package_details_seed();
     }
 
+    public function down()
+    {
 
-    public function down() {
-
-        $this->db->empty_table('packages_details');
+        $this->db->empty_table('package_details');
         $this->db->empty_table('modules');
         $this->db->empty_table('packages');
-        $this->dbforge->drop_table('subscriptions', TRUE);
-        $this->dbforge->drop_table('package_details', TRUE);
-        $this->dbforge->drop_table('modules', TRUE);
-        $this->dbforge->drop_table('packages', TRUE);
+        $this->dbforge->drop_table('subscriptions', true);
+        $this->dbforge->drop_table('package_details', true);
+        $this->dbforge->drop_table('modules', true);
+        $this->dbforge->drop_table('packages', true);
     }
 
-
-    public function packages() {
+    public function packages()
+    {
 
         $this->dbforge->add_field([
 
-            'id'              => [
+            'id' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 16
+                'type' => 'VARCHAR',
+                'constraint' => 16,
             ],
-            'name'            => [
+            'name' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 50
-            ]
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
 
         ]);
 
-        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_key('id', true);
 
-        return $this->dbforge->create_table('packages', TRUE);
+        return $this->dbforge->create_table('packages', true);
     }
 
-
-    public function modules() {
+    public function modules()
+    {
 
         $this->dbforge->add_field([
 
-            'id'              => [
+            'id' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 16
+                'type' => 'VARCHAR',
+                'constraint' => 16,
             ],
-            'name'            => [
+            'name' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 50
-            ]
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
 
         ]);
 
-        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_key('id', true);
 
-        return $this->dbforge->create_table('modules', TRUE);
+        return $this->dbforge->create_table('modules', true);
     }
 
-
-
-
-    public function package_details() {
+    public function package_details()
+    {
 
         $this->dbforge->add_field([
 
-            'package_id'      => [
+            'package_id' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 16
+                'type' => 'VARCHAR',
+                'constraint' => 16,
             ],
-            'module_id'       => [
+            'module_id' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 16
+                'type' => 'VARCHAR',
+                'constraint' => 16,
             ],
 
             'CONSTRAINT `package_details_ibfk_1` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE',
-            'CONSTRAINT `package_details_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`) ON DELETE CASCADE ON UPDATE CASCADE'
+            'CONSTRAINT `package_details_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`) ON DELETE CASCADE ON UPDATE CASCADE',
         ]);
 
-        $this->dbforge->add_key(['package_id', 'module_id'], TRUE);
+        $this->dbforge->add_key(['package_id', 'module_id'], true);
 
-        return $this->dbforge->create_table('package_details', TRUE);
+        return $this->dbforge->create_table('package_details', true);
     }
 
-
-    public function subscriptions() {
+    public function subscriptions()
+    {
 
         $this->dbforge->add_field([
 
-            'id'              => [
+            'id' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 11
+                'type' => 'VARCHAR',
+                'constraint' => 11,
             ],
-            'type'            => [
+            'type' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 50
+                'type' => 'VARCHAR',
+                'constraint' => 50,
             ],
-            'company_id'      => [
+            'company_id' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 11
+                'type' => 'VARCHAR',
+                'constraint' => 11,
             ],
-            'package_id'      => [
+            'package_id' => [
 
-                'type'           => 'VARCHAR',
-                'constraint'     => 16
+                'type' => 'VARCHAR',
+                'constraint' => 16,
             ],
-            'start_date'      => [
+            'start_date' => [
 
-                'type'           => 'DATE'
+                'type' => 'DATE',
             ],
             'expiration_date' => [
 
-                'type'           => 'DATE'
+                'type' => 'DATE',
             ],
 
             'CONSTRAINT `subscriptions_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE',
-            'CONSTRAINT `subscriptions_ibfk_2` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE'
+            'CONSTRAINT `subscriptions_ibfk_2` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE',
         ]);
 
-        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_key('id', true);
         $this->dbforge->add_key('company_id');
         $this->dbforge->add_key('package_id');
 
-        $this->dbforge->create_table('subscriptions', TRUE);
+        $this->dbforge->create_table('subscriptions', true);
     }
 
-
-    public function packages_seed() {
+    public function packages_seed()
+    {
 
         // ids have prefix of "PKG_" which means package
         $data = [
             [
                 'id' => 'PKG_PRP',
-                'name' => 'Project Package'
+                'name' => 'Project Package',
             ],
             [
                 'id' => 'PKG_SMP',
-                'name' => 'Shift Management Package'
+                'name' => 'Shift Management Package',
             ],
             [
                 'id' => 'PKG_HRA',
-                'name' => 'Human Resource Package'
+                'name' => 'Human Resource Package',
             ],
             [
                 'id' => 'PKG_PSP',
-                'name' => 'PayakApp Suite Package'
-            ]
+                'name' => 'PayakApp Suite Package',
+            ],
         ];
 
         return $this->db->insert_batch('packages', $data);
     }
 
-
-    public function modules_seed() {
+    public function modules_seed()
+    {
 
         // ids have prefix of "MDL_" which means module
         $modules = [
             [
                 'id' => 'MDL_DSB',
-                'name' => 'Dashboard'
+                'name' => 'Dashboard',
             ],
             [
                 'id' => 'MDL_PRJ',
-                'name' => 'Project'
+                'name' => 'Project',
             ],
             [
                 'id' => 'MDL_BBD',
-                'name' => 'Bulletin Board'
+                'name' => 'Bulletin Board',
             ],
             [
                 'id' => 'MDL_CHT',
-                'name' => 'Chat'
+                'name' => 'Chat',
             ],
             [
                 'id' => 'MDL_TKP',
-                'name' => 'Timekeeping'
+                'name' => 'Timekeeping',
             ],
             [
                 'id' => 'MDL_RSM',
-                'name' => 'Resume Management'
+                'name' => 'Resume Management',
             ],
             [
                 'id' => 'MDL_XPX',
-                'name' => 'Expense'
+                'name' => 'Expense',
             ],
             [
                 'id' => 'MDL_PTK',
-                'name' => 'Personal Tasks'
-            ]
+                'name' => 'Personal Tasks',
+            ],
         ];
 
         return $this->db->insert_batch('modules', $modules);
     }
 
+    public function package_details_seed()
+    {
 
-    public function package_details_seed() {
-        
         $data = [
 
             // MODULES OF PROJECT PACKAGE
             [
                 'package_id' => 'PKG_PRP',
-                'module_id' => 'MDL_DSB'
+                'module_id' => 'MDL_DSB',
             ],
             [
                 'package_id' => 'PKG_PRP',
-                'module_id' => 'MDL_PRJ'
+                'module_id' => 'MDL_PRJ',
             ],
             [
                 'package_id' => 'PKG_PRP',
-                'module_id' => 'MDL_BBD'
+                'module_id' => 'MDL_BBD',
             ],
             [
                 'package_id' => 'PKG_PRP',
-                'module_id' => 'MDL_PTK'
+                'module_id' => 'MDL_PTK',
             ],
             [
                 'package_id' => 'PKG_PRP',
-                'module_id' => 'MDL_CHT'
+                'module_id' => 'MDL_CHT',
             ],
 
             // MODULES OF SHIFT MANAGEMENT PACKAGE
             [
                 'package_id' => 'PKG_SMP',
-                'module_id' => 'MDL_DSB'
+                'module_id' => 'MDL_DSB',
             ],
             [
                 'package_id' => 'PKG_SMP',
-                'module_id' => 'MDL_TKP'
+                'module_id' => 'MDL_TKP',
             ],
             [
                 'package_id' => 'PKG_SMP',
-                'module_id' => 'MDL_BBD'
+                'module_id' => 'MDL_BBD',
             ],
             [
                 'package_id' => 'PKG_SMP',
-                'module_id' => 'MDL_PTK'
+                'module_id' => 'MDL_PTK',
             ],
             [
                 'package_id' => 'PKG_SMP',
-                'module_id' => 'MDL_CHT'
-            ]
+                'module_id' => 'MDL_CHT',
+            ],
         ];
 
         return $this->db->insert_batch('package_details', $data);
