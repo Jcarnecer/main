23b8f0e102c90be08be33c7a3eea3b52c5f72e4f
Mohammad Zahaire B. Guro <mohammadz.guro@astridtechnologies.com>
2018-03-28 21:53:16 +0800

    "Added users stats feature"

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,9 +1,10 @@
-		<script src="assets/js/jquery.js"></script>
 		<script src="assets/js/popper.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
 		<script src="assets/js/dashboard.js"></script>
 		<script src="assets/js/script.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
 		<!-- <script src="assets/js/jquery.nicescroll.min.js"></script> -->
+
+		<!-- Other Custom Scripts -->
+		<script src="assets/js/password-reset.js"></script>
 	</body>
 </html>
