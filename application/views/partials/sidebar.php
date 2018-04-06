beacd0ab98c2a9b2be380f23f9436d524c96fcae
jmacj <jm.nz0723@gmail.com>
2018-04-06 08:54:40 +0800

    added avatar

--- application/views/partials/sidebar.php
+++ application/views/partials/sidebar.php
@@ -102,24 +102,24 @@
 </div>
 <div class="main-content d-flex flex-column h-100">
 	<div class="topbar w-100" style="margin-bottom: -20px;">
-		<nav class="navbar navbar-custom navbar-expand-lg">
-			<div id="nav-icon-open" class="custom-toggle hidden-toggle">
-				<span></span>
-				<span></span>
-				<span></span>
-			</div>
+        <nav class="navbar navbar-custom navbar-expand-lg">
+            <div id="nav-icon-open" class="custom-toggle hidden-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
 			<a class="navbar-brand" href="#">PayakApps</a>
 			
 			<ul class="navbar-nav flex-row ml-auto">
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
-						<?= $user->first_name . " " . $user->last_name ?>
+						<img class="img-avatar mr-2" src="<?= $user->avatar_url ?>"><?= $user->first_name . " " . $user->last_name ?>
 					</a>
 					<div class="dropdown-menu dropdown-menu-right">
 						<a class="dropdown-item" href="<?= base_url('users/profile') ?>">My Profile</a>
 						<a class="dropdown-item" href="<?= base_url("users/profile/change-password") ?>">My Password</a>
 						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="<?= base_url("users/logout") ?>">Logout</a>
+						<a class="dropdown-item" href="<?= LOGOUT_URL ?>">Logout</a>
 					</div>
 				</li>
 				<!--
